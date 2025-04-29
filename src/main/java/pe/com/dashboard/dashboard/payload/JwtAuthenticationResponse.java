290f9cf10cf92c9021aed3d902a812e704e4a6fc
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-04-28 20:38:04 -0500

    Se simplifico el codigo haciendo uso de @Annotations

--- src/main/java/pe/com/dashboard/dashboard/payload/JwtAuthenticationResponse.java
+++ src/main/java/pe/com/dashboard/dashboard/payload/JwtAuthenticationResponse.java
@@ -1,41 +1,19 @@
 package pe.com.dashboard.dashboard.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor // Crea un constructor con todos los parámetros
 public class JwtAuthenticationResponse {
 
     private String token;
     private String username;
     private List<String> roles;
+    private String nombre;
 
-    public JwtAuthenticationResponse(String token, String username, List<String> roles) {
-        this.token = token;
-        this.username = username;
-        this.roles = roles;
-    }
-
-    // Getters y setters
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
 }
